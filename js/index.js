1d6d41b158aa9ae474ee4d9cb2f612d8ea51287f
Rodney <rodney.amani@student.moringaschool.com>
2025-03-25 23:12:49 +0300

    fetch recipes

--- js/index.js
+++ js/index.js
@@ -2,5 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchRecipes();
     const baseUrl = "http://localhost:3000/recipes";
   });
-  
+
+  function fetchRecipes() {
+    fetch("http://localhost:3000/recipes") 
+      .then(response => response.json())
+      .then(data => displayRecipes(data))
+      .catch(error => console.error("Error fetching recipes:", error));
+  }
 
