de47ba025afff77aa7085316f934dba24499edfe
Rodney <rodney.amani@student.moringaschool.com>
2025-03-26 08:13:52 +0300

    fetch

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const BaseUrl = "http://localhost:3000/recipes";
+
+
     const recipeContainer = document.getElementById("recipe-container");
     const modal = document.getElementById("recipe-modal");
     const modalTitle = document.getElementById("modal-title");
@@ -7,4 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const modalInstructions = document.getElementById("modal-instructions");
     const closeButton = document.querySelector(".close-btn");
 
+
+    // Fetch recipes from db.json
+    fetch(BaseUrl)
+        .then(response => response.json())
+        .then(recipes => {
+            recipes.forEach(recipe => {
+                const recipeCard = document.createElement("div");
+                recipeCard.classList.add("recipe-card");
+                recipeCard.innerHTML = `
+                    <img src="${recipe.image}" alt="${recipe.name}" class="recipe-img">
+                    <h3>${recipe.name}</h3>
+            
+
+                `
 });
+        
